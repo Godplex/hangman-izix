c4fc88406b5b6d00a77887a4b5fe42985449a251
Julian David Rincon Espinosa <julian1918@hotmail.es>
2022-02-05 21:02:28 -0500

    fix buttons

--- components/Keyboard.js
+++ components/Keyboard.js
@@ -9,38 +9,38 @@ class Keyboard extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
         <div class="keyboard-row pb-2">
-            <button class="btn btn-sm btn-primary" data-key="Q">Q</button>
-            <button class="btn btn-sm btn-primary" data-key="W">W</button>
-            <button class="btn btn-sm btn-primary" data-key="E">E</button>
-            <button class="btn btn-sm btn-primary" data-key="R">R</button>
-            <button class="btn btn-sm btn-primary" data-key="T">T</button>
-            <button class="btn btn-sm btn-primary" data-key="Y">Y</button>
-            <button class="btn btn-sm btn-primary" data-key="U">U</button>
+            <button class="btn btn-primary" data-key="Q">Q</button>
+            <button class="btn btn-primary" data-key="W">W</button>
+            <button class="btn btn-primary" data-key="E">E</button>
+            <button class="btn btn-primary" data-key="R">R</button>
+            <button class="btn btn-primary" data-key="T">T</button>
+            <button class="btn btn-primary" data-key="Y">Y</button>
+            <button class="btn btn-primary" data-key="U">U</button>
         </div>
         <div class="keyboard-row pb-2">
-            <button class="btn btn-sm btn-primary" data-key="I">I</button>
-            <button class="btn btn-sm btn-primary" data-key="O">O</button>
-            <button class="btn btn-sm btn-primary" data-key="P">P</button>
-            <button class="btn btn-sm btn-primary" data-key="A">A</button>
-            <button class="btn btn-sm btn-primary" data-key="S">S</button>
-            <button class="btn btn-sm btn-primary" data-key="D">D</button>
-            <button class="btn btn-sm btn-primary" data-key="F">F</button>
+            <button class="btn btn-primary" data-key="I">I</button>
+            <button class="btn btn-primary" data-key="O">O</button>
+            <button class="btn btn-primary" data-key="P">P</button>
+            <button class="btn btn-primary" data-key="A">A</button>
+            <button class="btn btn-primary" data-key="S">S</button>
+            <button class="btn btn-primary" data-key="D">D</button>
+            <button class="btn btn-primary" data-key="F">F</button>
         </div>
         <div class="keyboard-row pb-2">
-            <button class="btn btn-sm btn-primary" data-key="G">G</button>
-            <button class="btn btn-sm btn-primary" data-key="H">H</button>
-            <button class="btn btn-sm btn-primary" data-key="J">J</button>
-            <button class="btn btn-sm btn-primary" data-key="K">K</button>
-            <button class="btn btn-sm btn-primary" data-key="L">L</button>
-            <button class="btn btn-sm btn-primary" data-key="Z">Z</button>
-            <button class="btn btn-sm btn-primary" data-key="X">X</button>
+            <button class="btn btn-primary" data-key="G">G</button>
+            <button class="btn btn-primary" data-key="H">H</button>
+            <button class="btn btn-primary" data-key="J">J</button>
+            <button class="btn btn-primary" data-key="K">K</button>
+            <button class="btn btn-primary" data-key="L">L</button>
+            <button class="btn btn-primary" data-key="Z">Z</button>
+            <button class="btn btn-primary" data-key="X">X</button>
         </div>
         <div class="keyboard-row pb-2">
-            <button class="btn btn-sm btn-primary" data-key="C">C</button>
-            <button class="btn btn-sm btn-primary" data-key="V">V</button>
-            <button class="btn btn-sm btn-primary" data-key="B">B</button>
-            <button class="btn btn-sm btn-primary" data-key="N">N</button>
-            <button class="btn btn-sm btn-primary" data-key="M">M</button>
+            <button class="btn btn-primary" data-key="C">C</button>
+            <button class="btn btn-primary" data-key="V">V</button>
+            <button class="btn btn-primary" data-key="B">B</button>
+            <button class="btn btn-primary" data-key="N">N</button>
+            <button class="btn btn-primary" data-key="M">M</button>
         </div>
     `;
     }
